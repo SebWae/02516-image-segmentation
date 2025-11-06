4e0d749397f476336d9bdb6f41fd2416fb241c03
SebWae <s254120@dtu.dk>
2025-11-06 22:18:39 +0100

    initial commit

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+from sklearn.model_selection import KFold
+from torch.utils.data import DataLoader, Subset
+
 # this script should include a train function taking one of the models as input
 
 # applies 8-fold cross-validation for the PH2 dataset and 4-fold cross-validation for the DRIVE dataset
@@ -5,4 +8,28 @@
 # performance is reported as the average across all folds
 # applies early-stopping when validation loss increases (after x initial epochs) or only decreases by something smaller than a tolerance (e.g. e-4)
 
- 
+
+def train(k_folds, train_dataset, n_epochs=1000, random_state=42) -> dict:
+    out_dict = {"train_loss": [],
+                "train_dice": [],
+                "val_loss": [],
+                "val_dice": [],
+                "n_epochs": 0,
+                }
+    prev_val_loss = 1e10
+
+    kfold = KFold(n_splits=k_folds, shuffle=True, random_state=random_state)
+    for epoch in range(n_epochs):
+        for fold, (train_idx, val_idx) in enumerate(kfold.split(train_dataset)):
+            print(f"\n--- Fold {fold + 1}/{k_folds} ---")
+
+            # train and validation samplers for this fold
+            train_subsampler = Subset(train_dataset, train_idx)
+            val_subsampler = Subset(train_dataset, val_idx)
+
+            # initialize dataloaders
+            train_loader = DataLoader(train_subsampler, batch_size=4, shuffle=True)
+            val_loader = DataLoader(val_subsampler, batch_size=4, shuffle=False)
+
+
+    return out_dict
