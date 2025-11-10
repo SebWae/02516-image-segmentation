a48d018b9d6cf151e6af2f7fb7110de063f46e70
a-thansen <102360418+a-thansen@users.noreply.github.com>
2025-11-10 14:22:25 +0100

    added UNet

--- lib/model/UNetModel.py
+++ lib/model/UNetModel.py
@@ -1,2 +1,104 @@
-# this script should contain the UNet model as a class
-# similar to the models in the video classification project
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DoubleConv(nn.Module):
+    """(conv => ReLU) * 2 with padding to preserve spatial size"""
+
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.pool_conv = nn.Sequential(nn.MaxPool2d(2), DoubleConv(in_ch, out_ch))
+
+    def forward(self, x):
+        return self.pool_conv(x)
+
+
+class Up(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_ch, out_ch, kernel_size=2, stride=2)
+        self.conv = DoubleConv(out_ch * 2, out_ch)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = x2.size(2) - x1.size(2)
+        diffX = x2.size(3) - x1.size(3)
+        if diffY != 0 or diffX != 0:
+            x1 = F.pad(
+                x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2]
+            )
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    """
+    encoder: DoubleConv -> Down x3
+    bottleneck: DoubleConv
+    decoder: Up x3 -> OutConv
+    """
+
+    def __init__(self, in_channels=3, out_channels=1, features=(64, 128, 256, 512)):
+        super().__init__()
+        f1, f2, f3, f4 = features
+
+        self.inc = DoubleConv(in_channels, f1)
+        self.down1 = Down(f1, f2)
+        self.down2 = Down(f2, f3)
+        self.down3 = Down(f3, f4)
+
+        self.bottleneck = DoubleConv(f4, f4 * 2)
+
+        self.up3 = Up(
+            f4 * 2, f4
+        )  # 1024 -> up to 512, concat with x3 (512) -> conv(1024->512)
+        self.up2 = Up(f4, f3)  # 512 -> 256, concat with x2 (256)
+        self.up1 = Up(f3, f2)  # 256 -> 128
+        self.up0 = Up(f2, f1)  # 128 -> 64
+
+        self.outc = OutConv(f1, out_channels)
+
+    def forward(self, x):
+        # encoder / skip connections
+        x0 = self.inc(x)
+        x1 = self.down1(x0)
+        x2 = self.down2(x1)
+        x3 = self.down3(x2)
+
+        # bottleneck
+        b = self.bottleneck(x3)
+
+        # decoder with skip-connections (match sizes inside Up)
+        d3 = self.up3(b, x3)
+        d2 = self.up2(d3, x2)
+        d1 = self.up1(d2, x1)
+        d0 = self.up0(d1, x0)
+
+        out = self.outc(d0)
+        return out
