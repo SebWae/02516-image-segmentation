9bae299d4b9e6d5c1b8101f4e54e5596bd558ec2
SebWae <s254120@dtu.dk>
2025-11-13 21:01:47 +0100

    importing all evaluation functions and train_weak

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,4 @@
-from .eval_metrics import compute_dice
+from .eval_metrics import compute_dice, iou_score, accuracy_score, sensitivity_score, specificity_score, evaluate_all
 from .losses import FocalLoss, PixelWeightedCrossEntropyLoss
 from .train import train_model
+from .train_weak import train_model_weak
